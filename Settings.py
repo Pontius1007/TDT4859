0d597a694167f2d828d2663bbc2e6f9103f89d36
Pontius1007 <embrikeinang@msn.com>
2019-04-03 11:10:05 +0200

    Update

--- Settings.py
+++ Settings.py
@@ -3,16 +3,14 @@ class Settings:
     training_size = 0.8
     epochs = 250
     batch_size = 30
-    verbose = 1
+    verbose = 2
     n_splits = 5
 
     # Layer variables
     number_of_features = 11
     input_layer = 11
-    h_layer1 = 20
+    h_layer1 = 10
     h_layer2 = 10
-    h_layer3 = 0
-    h_layer4 = 0
     output_layer = 1
 
     # Random
